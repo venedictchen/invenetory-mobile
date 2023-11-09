430573e19413ccb7185003ac02e0aebcdb845be4
venedictchen <venedictchen@gmail.com>
2023-11-09 17:25:01 +0700

    refactor folder

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-
-class InventoryItem {
-  final String name;
-  final IconData icon;
-  final Color color;
-
-  InventoryItem(this.name, this.icon,this.color);
-}
+import 'package:invenetory_mobile/widgets/inventory_card.dart';
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
@@ -73,45 +66,3 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class InventoryCard extends StatelessWidget {
-  final InventoryItem item;
-
-  const InventoryCard(this.item, {super.key}); // Constructor
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: item.color,
-      child: InkWell(
-        onTap: () {
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-                content: Text("Kamu telah menekan tombol ${item.name}!")));
-        },
-        child: Container(
-          // Container Icon dan Text
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/widgets/inventory_card.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+
+class InventoryItem {
+  final String name;
+  final IconData icon;
+  final Color color;
+
+  InventoryItem(this.name, this.icon,this.color);
+}
+
+
+class InventoryCard extends StatelessWidget {
+  final InventoryItem item;
+
+  const InventoryCard(this.item, {super.key}); // Constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: item.color,
+      child: InkWell(
+        onTap: () {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(
+                content: Text("Kamu telah menekan tombol ${item.name}!")));
+        },
+        child: Container(
+          // Container Icon dan Text
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
