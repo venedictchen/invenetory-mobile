bd0b9af631d4aefea5e0b00a2f6a05e211691f05
venedictchen <venedictchen@gmail.com>
2023-11-09 17:31:47 +0700

    refactor folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:invenetory_mobile/menu.dart';
+import 'package:invenetory_mobile/screens/menu.dart';
 
 void main() {
   runApp(const MyApp());
